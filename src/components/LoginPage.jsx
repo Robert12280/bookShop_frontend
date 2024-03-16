10113700e8eee8445a2b5d3f84f4322cbd17a42f
Robert <robertyc.chang@wpgholdings.com>
2024-03-16 11:16:32 +0800

    update some error

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -65,7 +65,7 @@ const LoginPage = () => {
                     </p>
                     <a
                         className="googleBtn"
-                        href="http://localhost:3500/auth/google"
+                        href="https://bookshop-api-lb8f.onrender.com/auth/google"
                     >
                         <FcGoogle />
                     </a>
